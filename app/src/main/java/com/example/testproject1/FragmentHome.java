82ed1c8048ec31d71aa3e4346721ab87515bdf4d
2ISP19 <martel.member@gmail.com>
2021-11-13 14:57:35 +0700

    На данном этапе разработки приложение имеет следующие проблемы:
    1. Не работает регистрация пользователя через приложение (пока удаётся добавить пользователя через сайт Firebase).
    
    2. При успешном входе должна открываться новое активити, однако этого не происходит и приложение закрывается или переносит пользователя на форму регистрации. Это связано с тем, что в фрагментах есть активные кнопки (возможное решение проблемы: https://www.youtube.com/watch?v=y6I2Dwgtl80)
    
    На текущем этапе разработки других проблем не выявлено.

---
+++ app/src/main/java/com/example/testproject1/FragmentHome.java
@@ -0,0 +1,24 @@
+package com.example.testproject1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class FragmentHome extends Fragment implements View.OnClickListener {
+
+    Button btnchernobyl;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, null);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/testproject1/FragmentPerson.java
@@ -0,0 +1,17 @@
+package com.example.testproject1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentPerson extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_person, null);
+    }
+}
